931a7461fd4545286ac9ddf007f5c2bd3721ef5f
VEER <viratadak@gmail.com>
2022-11-02 13:36:43 +0530

    add max stock

---
+++ LC121 max stock.cpp
@@ -0,0 +1,35 @@
+/*
+LC121 Best Time to Buy and Sell Stock-
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ 
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min=INT_MAX,curr_profit=INT_MIN,max_profit=0;
+        for(int it=0;it<prices.size();it++){
+            if(min>prices[it])
+                min=prices[it];
+            
+            curr_profit=prices[it]-min;
+            
+            if(max_profit<curr_profit)
+                max_profit=curr_profit;
+        }
+        return max_profit;
+    }
+};
